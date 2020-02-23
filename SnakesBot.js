f3985d1155e770afa44d5211dc2f2bd23d5a9d74
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2020-02-22 16:57:34 -0800

    Starting out test

---
+++ SnakesBot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+const prefix = "-"
+
+Client.on('ready', ()=>{
+    console.log("The bot has been logged in!")
+})
+
+Client.on('message', (message)=>{
+    if(-message.content.startsWith.apply(prefix)) return;
+    if(message.author.bot) return;
+
+
+
+    if(message.content.startsWith(prefix + "twitch"))
+        message.reply("Snakes twitch channel is, https://twitch.tv/snakegamesttv (someone took snakegames so I had to use snakegamesttv)");
+
+    if(message.content.startsWith(prefix + "help"))
+        message.send.author("Snakes commands are, `-twitch, -youtube, -mcdiscord -mcleaks`.");
+
+
+    if(message.content.startsWith(prefix + "youtube"))
+        message.reply("Snakes youtube is, https://youtube.com/snakegamesofficial");
+
+    if(message.conent.startsWith(prefix + "mcdiscord"))
+        message.reply(Snakes mc server (also ran by his friend) is https://discord.gg/Q789y3N)
+
+    if(message.content.startsWith(prefix + "mcleaks"))
+        message.reply("The MC leaks website is; https://mcleaks.net If you want to know how to use it, here is snake's recent video on it! https://www.youtube.com/watch?v=XPtgMijhlQE");
+})
+
+
+client.login(process.env.token);
